a306d29b44da4604713801c0542a02e32d7945be
ayushkhanvilkar10 <khanvilkar.a@northeastern.edu>
2024-02-05 13:51:15 -0500

    Built a counter app

---
+++ counter-app/src/Counter.js
@@ -0,0 +1,17 @@
+import React, {useState} from 'react';
+
+const Counter = () => {
+
+    const [count, setCount] = useState(0);
+
+    return(
+        <div>
+            <h1>Push the buttons below to increment decrement the Counter - ${count}</h1>
+            <button onClick={() => setCount(count+1)}>Increment</button>
+            <button onClick={() => setCount(count-1)}>Decrement</button>
+        </div>
+    );
+} 
+
+export default Counter;
+
